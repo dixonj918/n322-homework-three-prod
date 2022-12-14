bf16ad6e52ebb8763dca427666d6565744aa1be5
dixonj918 <dixon.james@outlook.com>
2022-12-14 18:42:24 -0500

    updated to add homework-four UD portion to CR

--- src/components/toDO/toDo.js
+++ src/components/toDO/toDo.js
@@ -4,11 +4,36 @@ import Button from "react-bootstrap/Button";
 import styles from "./toDo.css";
 
 function ToDo({ todo }) {
+  const completeToDo = () => {
+    const todoref = firebase
+      .database()
+      .ref("n322-homework-three")
+      .child(todo.id);
+
+    todoref.update({
+      complete: !todo.complete,
+    });
+  };
+
+  const deleteToDo = () => {
+    const todoref = firebase
+      .database()
+      .ref("n322-homework-three")
+      .child(todo.id);
+
+    todoref.remove();
+  };
   return (
     <div>
-      <h1 className="fw-bold">{todo.title}</h1>
-      <Button variant="danger">Delete</Button>
-      <Button variant="success">complete</Button>
+      <h1 className={todo.complete ? "complete" : "incomplete"}>
+        {todo.title}
+      </h1>
+      <Button variant="danger" onClick={deleteToDo}>
+        Delete
+      </Button>
+      <Button variant="success" onClick={completeToDo}>
+        complete
+      </Button>
     </div>
   );
 }
